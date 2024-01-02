6e2d2a201c4de54744b1be1a8fcb558a114753a0
Raúl López <hi@raullg.com>
2024-01-02 19:23:42 +0000

    make facade

--- src/KnowledgeBase.php
+++ src/KnowledgeBase.php
@@ -2,6 +2,28 @@
 
 namespace Borah\KnowledgeBase;
 
+use Borah\KnowledgeBase\Client\KnowledgeBaseClient;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 class KnowledgeBase
 {
+    public function upsertOne(Model $model): bool
+    {
+        $client = new KnowledgeBaseClient();
+        return $client->upsert([$model->knowledgeInsertItem()]);
+    }
+
+    public function upsert(Collection $models): bool
+    {
+        $client = new KnowledgeBaseClient();
+        $data = $models->map(fn (Model $model) => $model->knowledgeInsertItem())->toArray();
+        return $client->upsert($data);
+    }
+
+    public function destroy(Model $model): bool
+    {
+        $client = new KnowledgeBaseClient();
+        return $client->destroy($model->knowledgeInsertItem()->id);
+    }
 }
--- src/Traits/BelongsToKnowledgeBase.php
+++ src/Traits/BelongsToKnowledgeBase.php
@@ -4,8 +4,8 @@ namespace Borah\KnowledgeBase\Traits;
 
 use Borah\KnowledgeBase\Client\KnowledgeBaseClient;
 use Borah\KnowledgeBase\DTO\KnowledgeInsertItem;
+use Borah\KnowledgeBase\Facades\KnowledgeBase;
 use Illuminate\Database\Eloquent\Model;
-
 use function Illuminate\Events\queueable;
 
 trait BelongsToKnowledgeBase
@@ -13,18 +13,15 @@ trait BelongsToKnowledgeBase
     public static function bootBelongsToKnowledgeBase()
     {
         static::created(queueable(function (Model $model) {
-            $client = new KnowledgeBaseClient();
-            $client->upsert([$model->knowledgeInsertItem()]);
+            KnowledgeBase::upsert($model);
         }));
 
         static::updated(queueable(function (Model $model) {
-            $client = new KnowledgeBaseClient();
-            $client->upsert([$model->knowledgeInsertItem()]);
+            KnowledgeBase::upsert($model);
         }));
 
         static::deleted(queueable(function (Model $model) {
-            $client = new KnowledgeBaseClient();
-            $client->destroy($model->knowledgeInsertItem()->id);
+            KnowledgeBase::destroy($model);
         }));
     }
 
